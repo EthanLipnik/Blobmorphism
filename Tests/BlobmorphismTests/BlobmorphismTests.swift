368c778b286165358c719f91ca9a4809cf25f3a8
Ethan Lipnik <ethanlipnik@gmail.com>
2021-07-24 18:12:33 -0400

    Removed tests

--- Tests/BlobmorphismTests/BlobmorphismTests.swift
+++ Tests/BlobmorphismTests/BlobmorphismTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import Blobmorphism
 
 final class BlobmorphismTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Blobmorphism().text, "Hello, World!")
-    }
 }
